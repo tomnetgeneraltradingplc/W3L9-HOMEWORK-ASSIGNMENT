134662d3ca25601c9d8ace8bc165a9f5b96b480e
tomnetgeneraltradingplc <tomnetgeneraltradingplc@gmail.com>
2018-03-15 15:18:57 -0500

    Lesson 8

---
+++ FPP-390-2018/W2L8-HOMEWORK ASSIGNMENT/Day2_Problem_2/EmployeeData.java
@@ -0,0 +1,5 @@
+package Day2_Problem_2;
+
+public interface EmployeeData {
+	double getSalary();
+}
---
+++ FPP-390-2018/W2L8-HOMEWORK ASSIGNMENT/Day2_Problem_3/Student.java
@@ -0,0 +1,6 @@
+package Day2_Problem_3;
+
+abstract public class Student {
+//implement
+	abstract public double computeGpa();
+}
